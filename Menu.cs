e4214d25706c8b990c63f0b5fb7eaee6c6419b4f
Jacque <jacquemueller2468@gmail.com>
2022-08-31 17:13:10 -0500

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    internal class Menu
+    {
+        public DateTime LastUpdated { get }   
+        public List<MenuItem> Items { get; set; }
+        public Menu(DateTime lastUpdated, List<MenuItem> items)
+        {
+            LastUpdated = lastUpdated;
+            Items = items;
+        }
+
+        public void PrintMenu(MenuItem item)
+        {
+            Console.WriteLine($"\n{item.Name}, ${item.Price}\n{item.Description}");
+            if (item.New)
+            {
+                Console.WriteLine("New Item");
+            }
+        }
+
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }    
+        public bool IsNewItem { get; set; }   
+
+        public MenuItem(string name, int price, string description, string category, bool isnewitem)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNewItem = isnewitem;
+        }
+    }
+}
